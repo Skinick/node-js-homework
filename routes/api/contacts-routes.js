bf34464c6dc9ce8755dff75ea04054adf369cc44
Skinick <107771280+Skinick@users.noreply.github.com>
2023-10-15 01:27:26 +0300

    middleware authenticate

--- routes/api/contacts-routes.js
+++ routes/api/contacts-routes.js
@@ -1,33 +1,45 @@
 const express = require("express");
 
 const { validateBody, ctrlWrapper } = require("../../utils");
-const { isValidId } = require("../../middlewares");
+const { isValidId, authenticate } = require("../../middlewares");
 const { contacts: ctrl } = require("../../controllers");
 
 const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
-router.get("/", ctrlWrapper(ctrl.getListContacts));
+router.get(
+  "/:contactId",
+  authenticate,
+  isValidId,
+  ctrlWrapper(ctrl.getContactId)
+);
 
-router.get("/:contactId", isValidId, ctrlWrapper(ctrl.getContactId));
 router.post(
   "/",
+  authenticate,
   validateBody(schemas.addSchema),
   ctrlWrapper(ctrl.addNewContact)
 );
 
 router.patch(
   "/:contactId/favorite",
+  authenticate,
   isValidId,
   validateBody(schemas.updateStatusSchema),
   ctrlWrapper(ctrl.updateStatusContact)
 );
 
-router.delete("/:contactId", isValidId, ctrlWrapper(ctrl.removeContactById));
+router.delete(
+  "/:contactId",
+  authenticate,
+  isValidId,
+  ctrlWrapper(ctrl.removeContactById)
+);
 
 router.put(
   "/:contactId",
+  authenticate,
   isValidId,
   validateBody(schemas.addSchema),
   ctrlWrapper(ctrl.updateContactById)
