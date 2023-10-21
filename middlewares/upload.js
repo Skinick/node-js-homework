2ce521adc4ee044c8645e42b274d67eb1a732d39
Skinick <107771280+Skinick@users.noreply.github.com>
2023-10-22 01:19:02 +0300

    rename file

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -6,28 +6,12 @@ const tempDir = path.join(__dirname, "../", "temp");
 const multerConfig = multer.diskStorage({
   destination: tempDir,
   filename: (req, file, cb) => {
-    // cb(null, file.originalname);
-    const uinquePreffix = `${Date.now()}_${Math.round(Math.random() * 1e9)}`;
-    const filename = `${uinquePreffix}_${file.originalname}`;
-    cb(null, filename);
+    cb(null, file.originalname);
   },
 });
 
-const limits = {
-  fileSize: 5 * 1024 * 1024,
-};
-
-const fileFilter = (req, file, cb) => {
-  if (file.originalname.split(".").pop() === "exe") {
-    cb(new Error("File extention not allow"));
-  }
-  cb(null, true);
-};
-
 const upload = multer({
   storage: multerConfig,
-  limits,
-  //fileFilter,
 });
 
 module.exports = upload;
