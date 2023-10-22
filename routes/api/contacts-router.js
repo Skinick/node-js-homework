5d9ec7df0668f3e955e5ad4d02a39e8e12354ae3
Skinick <107771280+Skinick@users.noreply.github.com>
2023-10-23 00:35:58 +0300

    rename avatar

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -2,14 +2,22 @@ import express from "express";
 
 import contactsController from "../../controllers/contacts-controller.js";
 
-import { authenticate, isEmptyBody, isValidId } from "../../middlewares/index.js";
+import {
+  authenticate,
+  upload,
+  isEmptyBody,
+  isValidId,
+} from "../../middlewares/index.js";
 
 import { validateBody } from "../../decorators/index.js";
 
-import { contactAddSchema, contactUpdateFavoriteSchema } from "../../models/Contact.js";
+import {
+  contactAddSchema,
+  contactUpdateFavoriteSchema,
+} from "../../models/Contact.js";
 
-const contactAddValidate = validateBody(contactAddSchema)
-const contactUpdateFavoriteValidate = validateBody(contactUpdateFavoriteSchema)
+const contactAddValidate = validateBody(contactAddSchema);
+const contactUpdateFavoriteValidate = validateBody(contactUpdateFavoriteSchema);
 
 const contactsRouter = express.Router();
 
@@ -19,11 +27,31 @@ contactsRouter.get("/", contactsController.getAll);
 
 contactsRouter.get("/:id", isValidId, contactsController.getById);
 
-contactsRouter.post("/", isEmptyBody, contactAddValidate, contactsController.add);
+// upload.fields([{name: "avatar", maxCount: 1}])
+// upload.array("avatar", 8)
+contactsRouter.post(
+  "/",
+  upload.single("avatar"),
+  isEmptyBody,
+  contactAddValidate,
+  contactsController.add
+);
 
-contactsRouter.put("/:id", isValidId, isEmptyBody, contactAddValidate, contactsController.updateById);
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  isEmptyBody,
+  contactAddValidate,
+  contactsController.updateById
+);
 
-contactsRouter.patch("/:id/favorite", isValidId, isEmptyBody, contactUpdateFavoriteValidate, contactsController.updateFavorite);
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  isEmptyBody,
+  contactUpdateFavoriteValidate,
+  contactsController.updateFavorite
+);
 
 contactsRouter.delete("/:id", isValidId, contactsController.deleteById);
 
